fe012b11a2981e195ddc4eedb4559957bc57c304
Ranpoes <2213321305@qq.com>
2026-06-07 01:34:43 +0800

    Complete Su-25

--- assets/ashvehicle_ywzj/scripts/su-25.js
+++ assets/ashvehicle_ywzj/scripts/su-25.js
@@ -1,2 +1,14 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder();
+    builder.setRotation("BLFlap", pitchInput * 12, 0, 0);
+    builder.setRotation("BRFlap", pitchInput * 12, 0, 0);
+    builder.setRotation("group", rollInput * 20, 0, 0);
+    builder.setRotation("group12", -rollInput * 20, 0, 0);
+    builder.setRotation("VBFlap", 0, -yawInput * 20, 0);
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput);
+    return builder;
+}
