a5ca129cc8f958ccddd3fff92a484a3d66b8d1d4
Ranpoes <2213321305@qq.com>
2026-06-21 22:50:41 +0800

    Complete YF-23

--- assets/ashvehicle_ywzj/scripts/yf-23.js
+++ assets/ashvehicle_ywzj/scripts/yf-23.js
@@ -1,2 +1,15 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder();
+    builder.setRotation("nozulL", pitchInput * 16, 0, 0);
+    builder.setRotation("nozulR", pitchInput * 16, 0, 0);
+    builder.setRotation("flapL", rollInput * 16, 0, 0);
+    builder.setRotation("flapR", -rollInput * 16, 0, 0);
+    builder.setRotation("flapLB", pitchInput * 16 + yawInput * 4, 0, 0);
+    builder.setRotation("flapRB", pitchInput * 16 - yawInput * 4, 0, 0);
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput);
+    return builder;
+}
