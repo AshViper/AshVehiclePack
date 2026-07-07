fb941214f038cf042c2e843df3ee35a7efcca5a1
Ranpoes <2213321305@qq.com>
2026-07-08 00:03:09 +0800

    Complete F-16

--- assets/ashvehicle_ywzj/scripts/f_16.js
+++ assets/ashvehicle_ywzj/scripts/f_16.js
@@ -1,2 +1,13 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder()
+    builder.setRotation("LeftTailPlane", pitchInput * 12, 0, 0)
+    builder.setRotation("RightTailPlane", pitchInput * 12, 0, 0)
+    builder.setRotation("LeftFlap", rollInput * 16, 0, 0)
+    builder.setRotation("RightFlap", -rollInput * 16, 0, 0)
+    builder.setRotation("verticalTail2", 0, -yawInput * 12, 0)
+    return builder
+}
