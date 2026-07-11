ab67d62152c6dd1b111e6f6e1b727a98f15b22f8
Ranpoes <2213321305@qq.com>
2026-07-11 16:00:03 +0800

    Complete Typhoon

--- assets/ashvehicle_ywzj/scripts/eurofighter.js
+++ assets/ashvehicle_ywzj/scripts/eurofighter.js
@@ -1,2 +1,14 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder();
+    builder.setRotation("flapLB", -pitchInput * 16, 0, 0);
+    builder.setRotation("flapRB", -pitchInput * 16, 0, 0);
+    builder.setRotation("flapL", rollInput * 16, 0, 0);
+    builder.setRotation("flapR", -rollInput * 16, 0, 0);
+    builder.setRotation("flapRV", 0, -yawInput * 16, 0);
+    builder.setRotation("Stick", -8 * pitchInput, 0, -8 * rollInput)
+    return builder;
+}
