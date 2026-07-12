20fa6177f97a352467f7816c48001012aa3dd5e5
Ranpoes <2213321305@qq.com>
2026-07-13 04:26:35 +0800

    Complete F-39E

--- assets/ashvehicle_ywzj/scripts/f-39e.js
+++ assets/ashvehicle_ywzj/scripts/f-39e.js
@@ -1,2 +1,14 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder();
+    builder.setRotation("FLFlap", -pitchInput * 16, 0, 0);
+    builder.setRotation("FRFlap", -pitchInput * 16, 0, 0);
+    builder.setRotation("LFlap", rollInput * 16, 0, 0);
+    builder.setRotation("RFlap", -rollInput * 16, 0, 0);
+    builder.setRotation("VBFlap", 0, -yawInput * 16, 0);
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput)
+    return builder;
+}
