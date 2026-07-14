73237d4dc47d67dd32097cb764bec8cd63693e1f
Ranpoes <2213321305@qq.com>
2026-07-15 00:14:48 +0800

    Complete MiG-29, Su-27, Su-33, Su-34, Su-57, X-47B, M777

--- assets/ashvehicle_ywzj/scripts/su-34.js
+++ assets/ashvehicle_ywzj/scripts/su-34.js
@@ -1,2 +1,16 @@
-// function prepareBones() {
-// }
+function updateBones(context) {
+    const pitchInput = context.getPitchInput()
+    const yawInput = context.getYawInput()
+    const rollInput = context.getRollInput()
+
+    const builder = createPoseBuilder()
+    builder.setRotation("FLFlap", -pitchInput * 12, 0, 0)
+    builder.setRotation("FRFLap", -pitchInput * 12, 0, 0)
+    builder.setRotation("BRFlap", pitchInput * 12, 0, 0)
+    builder.setRotation("BLFlap", pitchInput * 12, 0, 0)
+    builder.setRotation("LFlap", rollInput * 16, 0, 0)
+    builder.setRotation("RFlap", -rollInput * 16, 0, 0)
+    builder.setRotation("VBLFlap", 0, -yawInput * 12, 0)
+    builder.setRotation("VBRFlap", 0, -yawInput * 12, 0)
+    return builder
+}
